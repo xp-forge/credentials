566b527adf6172fbe2287a6b179518cf1a5c4456
Timm Friebe <thekid@thekid.de>
2018-01-12 11:00:32 +0100

    Implement support for Docker Secrets
    See https://docs.docker.com/engine/swarm/secrets/
    Implements #2

---
+++ src/test/php/security/credentials/unittest/FromDockerSecretsTest.class.php
@@ -0,0 +1,42 @@
+<?php namespace security\credentials\unittest;
+
+use security\credentials\FromDockerSecrets;
+use util\Secret;
+use lang\Environment;
+use io\Folder;
+use io\Path;
+
+class FromDockerSecretsTest extends AbstractSecretsTest {
+
+  /** @return security.credentials.Secrets */
+  protected function newFixture() {
+    return new FromDockerSecrets($this->path);
+  }
+
+  /** @return void */
+  public function setUp() {
+    $this->path= new Folder(Environment::tempDir(), 'secrets');
+    $this->path->exists() && $this->path->unlink();
+    $this->path->create(0777);
+
+    // Create fixtures
+    file_put_contents(new Path($this->path, 'test_db_password'), 'db');
+    file_put_contents(new Path($this->path, 'test_ldap_password'), 'ldap');
+    file_put_contents(new Path($this->path, 'prod_master_key'), 'master');
+
+    $subfolder= new Folder($this->path, 'xp/app');
+    $subfolder->create(0777);
+    file_put_contents(new Path($subfolder, 'mysql'), 'test');
+  }
+
+  /** @return void */
+  public function tearDown() {
+    $this->path->exists() && $this->path->unlink();
+  }
+
+  #[@test]
+  public function path() {
+    $path= new Path('.');
+    $this->assertEquals($path, (new FromDockerSecrets($path))->path());
+  }
+}
